f8203d2f7ad5d6e91d4acb3a1d6d68670c664094
= <vvampus@gmail.com>
2021-03-07 23:28:13 +0100

    Use qutebrowser and st

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,4 @@
 alias config='/usr/bin/git --git-dir=$HOME/dotfilesrepo/ --work-tree=$HOME'
-export MANPAGER="vim -c 'set ft=man' -"
 
 if status --is-login 
   exec startx &> /dev/null
@@ -13,3 +12,4 @@ set --unexport LINES
 # Vars
 export TERM=xterm-256color
 export EDITOR=vim
+export MANPAGER="vim -c 'set ft=man' -"
