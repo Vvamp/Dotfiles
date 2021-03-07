b31a36637b1edc76111aa94ecc90fa011a40d049
= <vvampus@gmail.com>
2021-03-07 01:48:33 +0100

    added birthday to hello

--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -3,8 +3,10 @@ function fish_greeting
  set _date_ (date +"%A %d-%m-%Y")
  set weeknr (date +"%V")
  set quote (fortune -s | sed 's/-- /\n    -- /')
+ set bday (python /home/vvamp/Coding/Tools/birthday.py)
  echo -e "It's $_date_ at$time"
  echo "Week #$weeknr"
+ echo "$bday"
  echo "Welcome, Vincent"
  echo ""
  #echo "\"$quote\""
